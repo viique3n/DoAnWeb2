b6f5dd96fd482c10c14ca0f550319a398c0dadac
Minh VAn <vii.que3n@gmail.com>
2020-06-12 12:35:52 +0700

    client init

---
+++ client/src/routes/test/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class About extends React.Component {
+    render(){
+      return ([
+        <div>
+          <h2>About</h2>
+          <p>Hello from react-router</p>
+        </div>
+      ]);
+    };
+};
---
+++ client/src/routes/test/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Dashboard extends React.Component {
+  render() {
+    return ([
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+    ]);
+  };
+};
