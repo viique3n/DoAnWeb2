3e0b7a8fd703ccb6c0755dad19bd1b4099622a9b
HoangSang <sanghuynhhoang1308@gmail.com>
2020-07-10 23:23:56 +0700

    Chinh sua giao dien co ban reactadmin

--- reactadmin/src/Components/Footer.js
+++ reactadmin/src/Components/Footer.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class Footer extends Component {
   render() {
+    
     return (
-      <div style={{ left: '40%', bottom: '10px', position: 'fixed' }}>
-        <p>------------------Footer------------------</p>
+      
+        <div style={{  bottom: '2px' }}>
+      <Navbar bg="light" expand="bl">
+          
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Internet Banking</h5>
+            <p>
+              Welcom!
+            </p>
+          </MDBCol>
+          <MDBCol md="15">
+            <h5 className="title">Admin</h5>
+            <ul>
+              <li className="list-unstyled">
+                <p>1760378-Trần Thị Cẩm Nhung</p>
+              </li>
+              <li className="list-unstyled">
+                <p>1760399-Phan Thị Lệ Quyên</p>
+              </li>
+              <li className="list-unstyled">
+                <p>1760402-Huỳnh Hoàng Sang</p>
+              </li>
+              <li className="list-unstyled">
+                <p>1760463-Nguyễn Minh Văn</p>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      
+    </Navbar>
+    <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="/"> DoAnWeb2 </a>
+        </MDBContainer>
       </div>
+      </div>
+      
     );
   }
 }
