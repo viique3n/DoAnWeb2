abb94a46c899a35b2c7eb9432b384bfd2ebd5e4f
1760399 <40138194+1760399@users.noreply.github.com>
2020-07-13 21:59:35 +0700

    Update giao dien reactadmin

--- reactadmin/src/App.js
+++ reactadmin/src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import { AuthRoute, UnAuthRoute } from "./Auth/AuthRoute";
-import HomePage from "./Pages/HomePage";
-import LoginPage from "./Pages/LoginPage";
-import ProfilePage from "./Pages/ProfilePage";
-import DanhSachKhachHangPage from "./Pages/DanhSachKhachHangPage";
-import CapNhatTaiKhoanPage from "./Pages/CapNhatTaiKhoanPage";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { AuthRoute, UnAuthRoute } from './Auth/AuthRoute';
+import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
+import ProfilePage from './Pages/ProfilePage';
+import DanhSachKhachHangPage from './Pages/DanhSachKhachHangPage';
+import CapNhatTaiKhoanPage from './Pages/CapNhatTaiKhoanPage';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
+      <div>
         <Switch>
           <Route exact path='/'>
             <HomePage />
